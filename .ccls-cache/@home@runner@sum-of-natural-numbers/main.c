42fc30febf9a30ce0c59cc589aa05d4917bb75bd
Sukhendra S Lodhi <95135906+sukhendralodhi@users.noreply.github.com>
2023-02-02 19:49:13 +0000

    add one more function that name is reverse_number that function return reverse value of given number using for loop.

--- .ccls-cache/@home@runner@sum-of-natural-numbers/main.c
+++ .ccls-cache/@home@runner@sum-of-natural-numbers/main.c
@@ -9,11 +9,21 @@ int sum_natural_numbers(int x) {
   return sum;
 }
 
+int reverse_number(int n) {
+  for(int i = n; i >= 1; i--) {
+    printf("%d\n",i);
+  }
+}
+
 int main(void) {
   int number;
   printf("Enter the number: ");
   scanf("%d",&number);
   int result = sum_natural_numbers(number); //calling function
-  printf("Sum = %d", result);
+  printf("Sum = %d\n", result);
+  printf("\n");
+  // reverse_number(number);
+  printf("Reverse number: \n");
+  printf("%d",reverse_number(number));
   return 0;
 }
